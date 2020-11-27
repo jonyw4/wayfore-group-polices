9a8ce3c08c624abcbbacf9bec9bf66733d81f019
Jonathan Célio <jonathan.clio@hotmail.com>
2020-11-27 17:18:36 +0000

    feat: update description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Wayfore - Políticas de Grupo</title>
         <link rel="icon" href="/favicon.ico" />
@@ -10,6 +10,12 @@ export default function Home() {
 
       <main>
         <h1>Políticas de Grupo da Wayfore</h1>
+        <p>
+          Essas são as regras de convivência para todos os grupos os quais são
+          gerenciados pela Wayfore nas redes sociais (WhatsApp, Facebook entre
+          outros)
+        </p>
+        <h2>É proibído</h2>
         <ol>
           <li>Enviar conteúdo pornográfico</li>
           <li>Fazer propaganda, sem a permissão dos moderadores</li>
@@ -28,6 +34,12 @@ export default function Home() {
           </li>
         </ol>
       </main>
+      <footer>
+        Para mais informações entre em contato nas{' '}
+        <a href="https://www.facebook.com/WayforeOfficial">
+          redes sociais da Wayfore
+        </a>
+      </footer>
     </div>
   );
 }
